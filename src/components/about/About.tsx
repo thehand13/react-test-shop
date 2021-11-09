f8930c317d204aaa47f0b32ae5b60a08c1a9fa69
Roman <thehand@yandex.ru>
2021-11-09 20:38:51 +0300

    v1.1.0
    *Basic styling added

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./About.module.css";
 
 const About: React.FC = () => {
-  return <div>About</div>;
+  return (
+    <Card>
+      <h2 className={classes.about}>About us</h2>
+      <p>
+        React Test Shop was founded in 2021 by developer Roman Petrov. He`s a
+        market leader of test shops ;)
+      </p>
+    </Card>
+  );
 };
 
 export default About;
